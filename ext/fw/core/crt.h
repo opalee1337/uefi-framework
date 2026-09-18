b1867cdbb1e417ecb2ee65a9d08e3d70c185fc6f
Opalee <108886090+opalee1337@users.noreply.github.com>
2026-09-19 01:06:06 +0200

    Delete ext/fw directory

--- ext/fw/core/crt.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "crt/memory/memory.h"
-#include "crt/string.h"
-#include "crt/new.h"
--- ext/fw/core/crt/new.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <cstddef>
-
-inline void* operator new  (std::size_t, void* p) noexcept { return p; }
-inline void* operator new[](std::size_t, void* p) noexcept { return p; }
-inline void  operator delete  (void*, void*) noexcept {}
-inline void  operator delete[](void*, void*) noexcept {}
